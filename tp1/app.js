3cd0dc8f019c08dd4890053ad5a03ff1bdb6745f
Karl MARQUES <kmarques@vetixy.com>
2022-01-03 13:16:12 +0100

    tp1 subject

---
+++ tp1/app.js
@@ -0,0 +1,5 @@
+/**
+ * Générer un tableau de 5x5 avec des chaînes par défaut sous la div #root
+ * En cliquant sur une case, on change le contenu en input contenant la valeur de la case
+ * En sortant de l'input (événement blur), on valide la valeur et on la met à jour dans le tableau
+ */
